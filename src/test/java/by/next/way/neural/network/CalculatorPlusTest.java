e8b4cba84ef5a5b77a31ac4862e5146451230d84
c.stupakevich <constantine.stupakevich@moneyman.ru>
2019-01-10 19:50:57 +0300

    Add more tests.

---
+++ src/test/java/by/next/way/neural/network/CalculatorPlusTest.java
@@ -0,0 +1,54 @@
+package by.next.way.neural.network;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+class CalculatorPlusTest {
+
+    private static Logger log = LogManager.getLogger();
+
+    private Random random;
+    private Double[][][] trainingSets;
+
+
+    @BeforeEach
+    void setUp() {
+        random = new Random();
+        trainingSets = new Double[][][]{
+                {{1.0, 0.0}, {1.0}},
+                {{0.0, 1.0}, {1.0}}
+        };
+    }
+
+    @Test
+    void example() {
+        log.info("NN training start!");
+        NeuralNetwork neuralNetwork = NeuralNetwork.create(trainingSets[0][0].length, 20, trainingSets[0][1].length);
+        for (int i = 0; i < 100000; i++) {
+            double first = random.nextDouble() / 2;
+            double second = random.nextDouble() / 2;
+            double sum = first + second;
+            neuralNetwork.train(Arrays.asList(first, second), Arrays.asList(sum));
+        }
+        log.info("NN training finish!");
+        print(neuralNetwork);
+        log.warn("error: " + neuralNetwork.calculateError(trainingSets));
+    }
+
+    static void print(NeuralNetwork neuralNetwork) {
+        log.info(
+                "0.32, 0.11 -> " + neuralNetwork.feedForward(Arrays.asList(0.32, 0.11)) + "\n" +
+                        "0.22, 0.11 -> " + neuralNetwork.feedForward(Arrays.asList(0.22, 0.11)) + "\n" +
+                        "0.2, 0.1 -> " + neuralNetwork.feedForward(Arrays.asList(0.2, 0.1)) + "\n" +
+                        "0.42, 0.11 -> " + neuralNetwork.feedForward(Arrays.asList(0.42, 0.11)) + "\n" +
+                        "0.1, 0.1 -> " + neuralNetwork.feedForward(Arrays.asList(0.1, 0.1)) + "\n" +
+                        "0.4, 0.13 -> " + neuralNetwork.feedForward(Arrays.asList(0.4, 0.13)) + "\n" +
+                        "0.7, 0.11 -> " + neuralNetwork.feedForward(Arrays.asList(0.7, 0.11))
+        );
+    }
+}
